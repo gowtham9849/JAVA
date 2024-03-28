aa758a8a1e6f87fa4544c598509c4707a2e62c59
gowtham9849 <155544023+gowtham9849@users.noreply.github.com>
2024-03-28 10:49:58 +0530

    Add files via upload

---
+++ arr.java
@@ -0,0 +1,14 @@
+package bankapk.jdbc.JdbcDao;
+
+public class arr {
+public static void main(String[]args) {
+ final int [] no = {1,2,3,4};
+ no[0]=9;
+ no[1]=10;
+ no[2]=50;
+ no[3]=100;
+ for(int i:no) {
+ System.out.println(i);
+ }	 
+ }
+}
